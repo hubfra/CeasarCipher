2d0879faedc5e42b1d67700b922e0e494b5ba338
hubfra <51798356+hubfra@users.noreply.github.com>
2020-03-22 20:25:23 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -7,25 +7,17 @@ using namespace std;
 
 int main()
 {
-	char alphabet[]{ 'A','B','C','D','E','F','G','H','I',
-					 'J','K','L','M','N','O','P','Q','R',
-					 'S','T','U','V','W','X','Y','Z','0',
-					 '1','2','3','4','5','6','7','8','9' };
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-
-	string fileName = "";
 	char action = '1';
-	short key = 0;
+	int key = 0;
 
-	ifstream input;
-	ofstream output;
+	ifstream input("input.txt");
+	ofstream output("output.txt");
 
-	cout << "Enter the file name: ";
-	cin >> fileName;
-	cout << "\nEncrypt - 1\nDecrypt - 2\n";
-	cout << "Choose action: ";
+	cout << "Encryption - 1\nDecryption - 2\nChoose action(1,2): ";
 	cin >> action;
-	cout << "\nEnter the key: ";
+	cout << "\nEnter key: ";
 	cin >> key;
 
 	key %= 36;
@@ -33,16 +25,10 @@ int main()
 	if (action == '2')
 		key = 36 - key;
 
-
-	input.open(fileName);
-	output.open("out" + fileName);
-
-	if (!input.is_open() || !output.is_open())
-		return 0;
-
 	char c;
 
-	while (input >> noskipws >> c)
+	input >> noskipws;
+	while (input >> c)
 	{
 		if (c >= 'a' && c <= 'z')
 			c -= 32;
@@ -54,7 +40,6 @@ int main()
 		else
 			output << c;
 	}
-
 
 	input.close();
 	output.close();
